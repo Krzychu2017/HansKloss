a4303a94c18d450ff98c37f4b6b2f4bb1995eb73
Krzysztof Puczkarski <krzysztof.puczkarski@gmail.com>
2017-09-03 19:46:03 +0200

    Karabiny Pułapki
    Karabin pułapka składa się z dwóch części, sensora i guzika, są one połączone w elemencie GunTrap. Należy je umieścić koło karabinów.

---
+++ Assets/GunTrap/ButtonController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+	public bool isDeactivated = false;
+	public Sprite btnDeactSprite;
+	public Sprite sensorDeactSprite;
+	public GameObject sensor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		//Debug.Log ("sensor triggered");
+		if (other.name == "Hans")
+		{
+			isDeactivated = true;
+			this.GetComponent<SpriteRenderer> ().sprite = btnDeactSprite;
+			sensor.GetComponent<SpriteRenderer> ().sprite = sensorDeactSprite;
+		}
+
+	}
+}
---
+++ Assets/GunTrap/SensorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SensorController : MonoBehaviour {
+
+	public GameObject button;
+
+
+	private ButtonController bt;
+	// Use this for initialization
+	void Start () {
+		bt  = button.GetComponent<ButtonController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		//Debug.Log ("sensor triggered");
+		if (!bt.isDeactivated && other.name == "Hans") {
+			SceneManager.LoadScene ("FinishScene", LoadSceneMode.Single);
+		}
+	
+	}
+}
